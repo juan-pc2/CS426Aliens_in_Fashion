a474e8872ef8b00bf83dfb48c1f63917b40863f2
Juan <juanpc2@uic.edu>
2021-04-07 01:13:39 -0500

    Teleporting and teleporters
    Added the code for teleporting and to ensure player meet the qualifications to move to the next planet. Also added teleporters from spawn to their respective planets and from the planets to spawn

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -6,22 +6,35 @@ public class Teleport : MonoBehaviour
 {
 
     private Transform destination;
+    private UIManage UIScript;
+
+    public GameObject UIObject;
     public GameObject destinationPortal;
+    public bool alwaysOpen;
 
     // Start is called before the first frame update
     void Start()
     {
         destination = destinationPortal.GetComponent<Transform>();
+        UIScript = UIObject.GetComponent<UIManage>();
+        alwaysOpen = true;
     }
 
     void OnTriggerEnter(Collider player)
     {
+        if (alwaysOpen || openPortal())
+        {
             player.transform.position = new Vector3(destination.position.x, destination.position.y, destination.position.z);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    bool openPortal()
     {
+        if (UIScript.numAliensHired >= 1 && UIScript.fashionItems >= 3)
+            return true;
 
+        return false;
     }
 }
+
+
